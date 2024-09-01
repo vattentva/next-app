9ae240316ee35fb6d38f9a4c0268dac2a0bfcb7e
vattentva <vattentva.d3@gmail.com>
2024-09-01 19:39:29 +0900

    chore: build test

--- app/dashboard/route-handler.tsx
+++ app/dashboard/route-handler.tsx
@@ -9,26 +9,28 @@ export default async function Sample() {
 
     // display data from route handler
     const data = await getData()
+    console.log(data);    
 
-    const renderNode = (obj: Object) => {
-        return Object.entries(obj).map(([key, value]) => {
-            if (typeof value === 'object') {
-                return (
-                    <div className="flex flex-col" key={key}>
-                        <code>[{key}]</code>
-                        {renderNode(value)}
-                    </div>
-                )
-            } else {
-                return <code key={key}>{key}: {value}</code>
-            }
-        })
-    }
+    // const renderNode = (obj: Object) => {
+    //     return Object.entries(obj).map(([key, value]) => {
+    //         if (typeof value === 'object') {
+    //             return (
+    //                 <div className="flex flex-col" key={key}>
+    //                     <code>[{key}]</code>
+    //                     {renderNode(value)}
+    //                 </div>
+    //             )
+    //         } else {
+    //             return <code key={key}>{key}: {value}</code>
+    //         }
+    //     })
+    // }
 
     return  (
         <div className="flex flex-col">
             {/* <p>{JSON.stringify(data)}</p> */}
-            {renderNode(data)}
+            {/* {renderNode(data)} */}
+            TEST
         </div>
     )
 }
