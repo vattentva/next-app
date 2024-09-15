12f7acd32f8bab9657cc16c9db77d33936200eac
vattentva <vattentva.d3@gmail.com>
2024-09-15 17:29:57 +0900

    chore: development deploy test

--- components/dashboard/SampleData.tsx
+++ components/dashboard/SampleData.tsx
@@ -3,20 +3,21 @@ import { getSampleData } from '@/infrastructure/api/getSampleData';
 import { ErrorResponse, isErrorResponse } from '@/shared/types/httpResponse';
 
 export async function SampleData() {
-  const data: UserInfo|ErrorResponse = await getSampleData();
-  console.log('🚀 ~ SampleData ~ data:', data);
-  if (isErrorResponse(data)) {
-    return <p>Error: {data.status} - {data.message}</p>;
-  }
+  // const data: UserInfo|ErrorResponse = await getSampleData();
+  // console.log('🚀 ~ SampleData ~ data:', data);
+  // if (isErrorResponse(data)) {
+  //   return <p>Error: {data.status} - {data.message}</p>;
+  // }
 
   return  (
     <>
-      <p>{data.request.origin}</p>
+      <h1>Dashborad Page</h1>
+      {/* <p>{data.request.origin}</p>
       <p>{data.request.pathname}</p>
       <p>{data.request.search}</p>
       <p>{data.method}</p>
       <p>{data.userAgent}</p>
-      <p>{data.currentTime}</p>
+      <p>{data.currentTime}</p> */}
     </>
   );
 }
