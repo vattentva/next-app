e8b37dbac07994301b17c846ae19e74e2014d3e1
vattentva <vattentva.d3@gmail.com>
2024-09-15 18:00:21 +0900

    fix: /login redirect with middleware.ts

--- components/common/Main.tsx
+++ components/common/Main.tsx
@@ -2,9 +2,9 @@
 
 export function Main({ children }: { children: React.ReactNode }) {
   return (
-    <main className="flex-1 p-4">
+    <main className="flex-1">
       <div className="flex flex-col">
-        <div className="bg-gray-400">
+        <div className="bg-gray-500">
           {children}
         </div>
       </div>
--- components/dashboard/SampleData.tsx
+++ components/dashboard/SampleData.tsx
@@ -1,23 +1,5 @@
-import { UserInfo } from '@/domain/models/Sample';
-import { getSampleData } from '@/infrastructure/api/getSampleData';
-import { ErrorResponse, isErrorResponse } from '@/shared/types/httpResponse';
-
 export async function SampleData() {
-  // const data: UserInfo|ErrorResponse = await getSampleData();
-  // console.log('🚀 ~ SampleData ~ data:', data);
-  // if (isErrorResponse(data)) {
-  //   return <p>Error: {data.status} - {data.message}</p>;
-  // }
-
   return  (
-    <>
-      <h1>Dashborad Page</h1>
-      {/* <p>{data.request.origin}</p>
-      <p>{data.request.pathname}</p>
-      <p>{data.request.search}</p>
-      <p>{data.method}</p>
-      <p>{data.userAgent}</p>
-      <p>{data.currentTime}</p> */}
-    </>
+    <h1>Dashboard Page</h1>
   );
 }
