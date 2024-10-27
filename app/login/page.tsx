ad7df051c191955a3d9389aabc5b7d8d12a2a328
vattentva <vattentva.d3@gmail.com>
2024-10-27 18:00:29 +0900

    feat: add prisma client and migration

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,7 @@
 'use client';
 import GoogleButton from '@/components/ui/GoogleButton';
 import React from 'react';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { handleRedirect } from '@/shared/actions';
 
 export default function Page() {
-  const { data: session } = useSession();
-  if (session?.user) {
-    handleRedirect('/');
-  }
   return <GoogleButton />;
 }
--- auth.ts
+++ auth.ts
@@ -1,8 +1,43 @@
 import NextAuth, { type AuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from './prisma';
 
+const convert = (unixTimeInSeconds) => new Date(unixTimeInSeconds * 1000).toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' });
+
+// https://next-auth.js.org/configuration/options
 const options: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: 'jwt',
+    maxAge: 1 * 1 * 60 * 60, // 1 hour
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_SECRET,
+  },
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log('signIn()', profile);
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return '/';
+    },
+    // persist the data in the token
+    async jwt({ token, account, profile }) {
+      // console.log(token, account, profile);
+      console.log('jwt()', convert(token.iat));
+      console.log('jwt()', convert(token.exp));      
+      
+      return token;
+    },
+    // pass the data through to the browser
+    async session({ session, user, token }) {      
+      return session;
+    },
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID ?? '',
